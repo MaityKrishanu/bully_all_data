ff44c730ecc0d21b4ae436a7700c565cececc559
MaityKrishanu <53084622+MaityKrishanu@users.noreply.github.com>
2021-02-14 00:19:21 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,73 @@
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+#from flask_cors import CORS
+import pickle
+import time
+import numpy as np
+from keras.preprocessing.sequence import pad_sequences
+from keras.models import model_from_json 
+import pickle
+app = Flask(__name__)
+#CORS(app)
+vocab_size = 12708
+maxlen = 30
+# loading
+with open('tokenizer1.pickle', 'rb') as handle:
+    tokenizer1 = pickle.load(handle)
+
+
+#model = pickle.load(open('model.pkl', 'rb'))
+json_file = open('model1.json','r')
+loaded_model_json = json_file.read()
+json_file.close()
+
+# use Keras model_from_json to make a loaded model
+
+loaded_model = model_from_json(loaded_model_json)
+
+# load weights into new model
+
+loaded_model.load_weights("model1.h5")
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict',methods=['GET','POST']) #'GET',
+def predict():
+    '''
+    For rendering results on HTML GUI
+    '''
+    if request.method == 'POST':
+      form = request.form
+      user_input = form['paragraph']
+    #temp=request.get_data(as_text=True)
+    start = time.time()
+    #new_string = bert_abstract.replace("+", " ")
+    #new_string = new_string.replace("%2C", " ")
+    #new_string = new_string.replace("%3B", " ")
+    #new_string = new_string.replace("text_input=", " ")
+    #new_string= " ".join(new_string.split())
+    #print(temp)
+    new=[user_input]
+    tok = tokenizer1.texts_to_sequences(new)
+    pad = pad_sequences(tok, padding='post', maxlen=maxlen)
+    y_pred1 = loaded_model.predict(pad)
+    end = time.time()
+    pred = np.argmax(y_pred1, axis=-1)
+    if pred == 1:
+        output = "Bully"
+    else:
+        output = "Non Bully"
+
+    tt = end - start
+    cc = "all bal chal"
+    return render_template('index.html', Input_text1='Input Sentence : {}'.format(user_input),
+                           prediction_text='Input Sentence Indicates : {}'.format(output)
+                           , Time_taken='Time Taken for Prediction : {:.2f}'.format(tt),
+                           nonbully_prob ='Non-Bully  Probability : {:.2f}'.format(y_pred1[0,0]),
+                           bully_prob='Bully Probability : {:.2f}'.format(y_pred1[0,1]))
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
